a14acb92417c8072ca5d3526b6d1bac2c03ed10d
supertitas <151513278+supertitas@users.noreply.github.com>
2024-07-06 21:25:14 +0800

    Delete Code and Data for WRR/Code directory

--- Code and Data for WRR/Code/CICLAR/attention.py
+++
@@ -1,9 +0,0 @@
-from keras.layers import Permute, Dense, Multiply
-# 定义注意力机制层
-
-def attention_layer(inputs, time_steps):
-    a = Permute((2, 1))(inputs)
-    a = Dense(time_steps, activation='softmax')(a)
-    a_probs = Permute((2, 1), name='attention_vec')(a)
-    output_attention_mul = Multiply()([inputs, a_probs])
-    return output_attention_mul
